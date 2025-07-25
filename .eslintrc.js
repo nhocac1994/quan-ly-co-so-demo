f7a662e567e4615b72d0918e7c9ca2ebf963709b
Quyền Nguyễn <quyennguyen@192.168.2.182>
2025-07-25 14:51:54 +0700

    Add ESLint config to treat warnings as warnings instead of errors

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  }
+}; 
